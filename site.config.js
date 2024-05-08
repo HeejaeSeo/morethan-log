a433130a61c5fa03cf646880d677f631c7ef50f3
HeejaeSeo <49803890+HeejaeSeo@users.noreply.github.com>
2024-05-09 00:31:33 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "rendezvous81",
-    image: "/me.png", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: "/me2.png", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "frontend developer",
     bio: "I develop everything using node.",
     email: "chungyounglove@gmail.com",
